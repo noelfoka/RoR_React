93a31a7729e2c1da1012204ecf737dc01d903fd5
noelfoka <nomgnenoel@gmail.com>
2023-11-27 19:40:08 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,44 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+airlines = Airline.create([
+  { 
+    name: "united airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  }, 
+  { 
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  { 
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+  }, 
+  { 
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+  }, 
+  { 
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+  }, 
+  { 
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+  }
+])
+
+reviews = Review.create([
+    {
+        title: "Geat airline",
+        description: "I had a lovely time.",
+        score: 5,
+        airline: airlines.first
+    },
+    {
+        title: "Bad airline",
+        description: "I had a bad time.",
+        score: 1,
+        airline: airlines.first
+    }
+])
